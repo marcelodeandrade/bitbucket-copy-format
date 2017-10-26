254c1b4b42ef6e61e25be014360e7bdc4459468d
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2017-10-26 20:10:05 -0300

    Add notification

--- src/background.js
+++ src/background.js
@@ -1,5 +1,25 @@
-"use strict";
+(browser => {
+    "use strict";
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, {onClicked: true}, (response) => {});
-});
+    browser.browserAction.onClicked.addListener(tab => {
+
+        browser.tabs.executeScript(null, {file: "src/content.script.js"});
+
+        browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+            console.log(request);
+
+            browser.notifications.create('bitbucket-copy-format', {
+                type: 'basic',
+                iconUrl: 'src/images/icon.png',
+                title: 'bitbucket-copy-format',
+                message: 'Copied'
+            }, () => {
+                sendResponse({
+                    "response": "notificated"
+                });
+            });
+
+        });
+    });
+
+})(chrome||browser);
--- src/content.script.js
+++ src/content.script.js
@@ -1,35 +1,35 @@
-"use strict";
+(browser => {
+    "use strict";
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    
-    if(request.onClicked) {
+    const commitQuery = document.querySelector(".commit-message").textContent.split(/(?=[\d]{5,7})/);
+    const commitMessage = commitQuery[0] || "Sem informações";
+    const commitTask = commitQuery[1] || "";
+    const commitLink = document.querySelector("a.changeset-hash");
+    const commitBranch = document.querySelector(".commit-branches > a");
 
-        const commitQuery = document.querySelector(".commit-message").textContent.split(/(?=[\d]{5,7})/);
-        const commitMessage = commitQuery[0] || "Sem informações";
-        const commitTask = commitQuery[1] || "";
-        const commitLink = document.querySelector("a.changeset-hash");
-        const commitBranch = document.querySelector(".commit-branches > a");
+    const toCopy = `
+    ${commitMessage}
 
-        const toCopy = `
-        ${commitMessage}
+    "task: #${commitTask}":http://tickets.id5.com.br:3001/issues/${commitTask}
+    "commit: ${commitLink.textContent}":${commitLink.href}
+    "branch: ${commitBranch.textContent.trim()}":${commitBranch.href}
+    `;
 
-        "task: #${commitTask}":http://tickets.id5.com.br:3001/issues/${commitTask}
-        "commit: ${commitLink.textContent}":${commitLink.href}
-        "branch: ${commitBranch.textContent.trim()}":${commitBranch.href}
-        `;
+    const dummyElement = document.createElement("textArea");
 
-        const dummyElement = document.createElement("textArea");
+    dummyElement.setAttribute("name", "dummyElement");
+    dummyElement.value = toCopy;
 
-        dummyElement.setAttribute("name", "dummyElement");
-        dummyElement.value = toCopy;
+    document.body.appendChild(dummyElement);
 
-        document.body.appendChild(dummyElement);
+    dummyElement.select();
 
-        dummyElement.select();
+    document.execCommand("copy");
 
-        document.execCommand("copy");
+    document.body.removeChild(dummyElement);
 
-        document.body.removeChild(dummyElement);
-    }
+    browser.runtime.sendMessage({"message": toCopy}, function(response) {
+        console.log(response);
+    });
 
-});
+})(chrome||browser);
