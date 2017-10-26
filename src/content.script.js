838bf4ea7d63d484bc7aec78b09eb7bed74a6a91
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2017-10-26 20:44:20 -0300

    Fix correct commit hash

--- src/content.script.js
+++ src/content.script.js
@@ -4,14 +4,14 @@
     const commitQuery = document.querySelector(".commit-message").textContent.split(/(?=[\d]{5,7})/);
     const commitMessage = commitQuery[0] || "Sem informações";
     const commitTask = commitQuery[1] || "";
-    const commitLink = document.querySelector("a.changeset-hash");
+    const commitLink = document.querySelector("span.changeset-hash");
     const commitBranch = document.querySelector(".commit-branches > a");
 
     const toCopy = `
     ${commitMessage}
 
     "task: #${commitTask}":http://tickets.id5.com.br:3001/issues/${commitTask}
-    "commit: ${commitLink.textContent}":${commitLink.href}
+    "commit: ${commitLink.textContent}":https://bitbucket.org/id5-web/websad-extranet/commits/${commitLink.textContent}
     "branch: ${commitBranch.textContent.trim()}":${commitBranch.href}
     `;
 
