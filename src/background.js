bf40bd029e9706a0b01edef62856444711e44f57
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2017-11-22 16:58:06 -0300

    Load storaged options

--- src/background.js
+++ src/background.js
@@ -1,13 +1,15 @@
+/*jshint esversion: 6 */
+
 (browser => {
     "use strict";
-
+    
     browser.browserAction.onClicked.addListener(tab => {
-
+        
         browser.tabs.executeScript(null, {file: "src/content.script.js"});
-
+        
         browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             console.log(request);
-
+            
             browser.notifications.create('bitbucket-copy-format', {
                 type: 'basic',
                 iconUrl: 'src/images/icon.png',
@@ -17,11 +19,25 @@
                 sendResponse({
                     "response": "notificated"
                 });
+                                
+                browser.storage.local.get("autoclose", (items) => {
+                    
+                    if (!browser.runtime.error) {
 
-                setTimeout(() => { chrome.notifications.clear(id); }, 1000);
+                        if(items.autoclose) {
+                            browser.tabs.query({ active: true }, (tabs) => {
+                                chrome.tabs.remove(tabs[0].id);
+                            });
+                        }
+
+                    }
+
+                });
+                
+                setTimeout(() => { browser.notifications.clear(id); }, 1000);
             });
-
+            
         });
     });
-
+    
 })(chrome||browser);
--- src/options.js
+++ src/options.js
@@ -1,23 +1,39 @@
+/*jshint esversion: 6 */
+
 (browser => {
     "use strict";
+    
+    function loadOptions() {
+        browser.storage.local.get({
+            autoclose: 'autoclose'
+        }, function(items) {
+
+            const id = (items.autoclose) ? "on" : "off";
+            const radio = document.getElementById(id);
+            radio.setAttribute("checked", "checked");
+            parent = radio.parentNode;
+            parent.classList.add('active');
+        });
+    }
     
     function saveOptions() {
         
         const autoclose = document.querySelector('[name="autoclose"').checked;
-        console.log(autoclose);
         
-        browser.storage.sync.set({
+        browser.storage.local.set({
             autoclose: autoclose
         }, 
         () => {
             const status = document.getElementById('status');
             status.textContent = 'Options saved.';
+            
             setTimeout(function() {
                 status.textContent = '';
-            }, 750);
+            }, 1000);
         });
     }
     
+    document.addEventListener('DOMContentLoaded', loadOptions);
     document.getElementById('save').addEventListener('click', saveOptions);
     
 })(chrome||browser);
